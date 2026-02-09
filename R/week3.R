7dad9fa69c417da5dfdc80e698c28b2b84048148
Zachariah R. Roper <roper063@umn.edu>
2026-02-09 12:09:21 -0600

    Added freq tables list, barplots, and count.

--- R/week3.R
+++ R/week3.R
@@ -11,5 +11,8 @@ clean_df <- clean_df[clean_df$q6 == 1,]
 # Analysis
 clean_df$timeSpent <- difftime(clean_df$timeEnd, clean_df$timeStart, units = "secs")
 hist(clean_df$timeSpent <- as.numeric(clean_df$timeSpent))
-
-
+frequency_tables_list <- lapply(clean_df[,5:14], table)
+lapply(frequency_tables_list, barplot)
+sum(clean_df$q1 >= clean_df$q2 & clean_df$q2 != clean_df$q3)
+for (i in 1:length(frequency_tables_list)) {
+  barplot(frequency_tables_list[[i]])}
