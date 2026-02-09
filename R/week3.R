7d4e7bf6358eae8439a288ab4fb95311bf611d57
Zachariah R. Roper <roper063@umn.edu>
2026-02-09 11:20:05 -0600

    Cleaned dataset

--- R/week3.R
+++ R/week3.R
@@ -5,7 +5,7 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 raw_df <- read.csv(file = "../data/week3.csv")
 raw_df$timeStart <- as.POSIXct(raw_df$timeStart)
 raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd)
-
-
+clean_df <- raw_df[format(raw_df$timeStart, "%Y-%m") != "2017-06", ]
+clean_df <- clean_df[clean_df$q6 == 1,]
 
 # Analysis
